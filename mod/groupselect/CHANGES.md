29cb8fab9d03ba46c435665e1ed45f9099a7a985
root <root@moodle.levinsky.ac.il>
2017-07-16 23:49:56 +0300

    mod/groupselect (new)

---
+++ mod/groupselect/CHANGES.md
@@ -0,0 +1,14 @@
+# Changes for mod_groupselect
+
+## Version 3.13 (2017061206)
+
+*   Better error messages for the module administration (via @phish108)
+*   Allow to configure participation in multiple groups (fixes issue #8, via @phish108)
+*   Allow administrators and teachers to set supervision roles (fixes bug #14, via @phish108)
+*   Omit including unnecessary libraries (fixes bug #10, via @lucaboesch)
+*   Better conformity with Moodle Coding and Packaging Standards
+
+## Older Versions
+
+Older Version did not document their changes. The change log starts with
+version 3.13.
---
+++ mod/groupselect/lang/de/groupselect.php
@@ -0,0 +1,18 @@
+<?php
+
+$string['assignedteacher'] = 'Betreuung';
+$string['eventgroupteacheradded'] = 'Betreuung zugeordnet';
+$string['eventexportlinkcreated'] = 'Exportlink erstellt';
+$string['maxgroupmembership'] = 'Maximale Anzahl der Gruppenteilnahme pro Teilnehmenden ';
+$string['maxgroupmembership_error_low'] = 'Fehler: Es muss mindestens eine (1) Gruppe wählbar sein!';
+$string['modulename_help'] = '<p>Teilnehmer/innen können Gruppen erstellen und wählen: </p><ul><li>Teilnehmer/innen können Gruppen erstellen, diesen eine Beschreibung geben und, falls gewünscht, mit einem Passwort schützen</li><li>Teilnehmer/innen können Gruppen auswählen und betreten</li><li>Trainer/innen ohne Bearbeitungsrechte können Gruppen hinzugefügt werden</li><li>Trainer/in kann die Gruppenliste des Kurses als CSV-Datei exportieren</li><li>Volle Kompatibilität zu Basis Moodle-Gruppen: Gruppen können auch durch andere Plugins erzeugt werden.</li></ul>';
+$string['notifyexpiredselection'] = 'Zeige Meldung, wenn das Einschreibeende vorüber ist';
+$string['notifyexpiredselection_help'] = 'Wenn gesetzt, wird eine Meldung angezeigt falls das Einschreibeende vorüber ist';
+$string['studentcansetenrolmentkey'] = 'Teilnehmende können Passwörter setzen um Gruppen beizutreten';
+$string['studentcansetenrolmentkey_help'] = 'Wenn gesetzt, kann ein/e Teilnehmer/in ein Gruppenpasswort setzen';
+$string['studentcansetgroupname'] = 'Teilnehmende dürfen Gruppennamen selbst bestimmen.';
+$string['studentcansetgroupname_help'] = 'Wenn gesetzt, kann ein/e Teilnehmer/in einen Gruppennamen setzen';
+$string['supervisionrole'] = 'Rolle für die Betreuung';
+$string['supervisionrole_help'] = 'Supervisorenrolle festlegen für die Betreuung der Gruppen festlegen (Standard: Lehrer ohne Bearbeitungsrecht)';
+$string['timeavailable_error_past_timedue'] = 'Fehler: Aktivität kann nicht nach dem Ende anfangen!';
+$string['timedue_error_pre_timeavailable'] = 'Fehler: Aktivität kann nicht vor dem Start enden!';
