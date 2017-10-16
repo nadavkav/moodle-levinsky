c2fb79b64f31bc44a84b41006c4740f9bceefabe
Nadav Kavalerchik <nadavkav@gmail.com>
2017-10-16 10:30:12 +0300

    repository/wpvod (new) m31 version - patched to work with levinsky vod server

---
+++ repository/wpvod/oauth_callback.php
@@ -0,0 +1,8 @@
+<?php
+
+// http://services.levinsky.ac.il/wp-vod/wp-admin/users.php?page=rest-oauth1-apps&action=edit&id=58
+// Key: fflUdG6wSVnG
+// Secret: YuVclbc6iQWAw31MzvqmkCX953pd1zWQ6gUVLWPN6ujkDrHi
+
+echo "Callback";
+var_dump($_REQUEST);
