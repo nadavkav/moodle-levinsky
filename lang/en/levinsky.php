600caf4553111c1570e067772e76f285a4028883
Nadav Kavalerchik <nadavkav@gmail.com>
2018-09-28 21:58:58 +0300

    lang/en (translation) Levinsky strings

---
+++ lang/en/levinsky.php
@@ -0,0 +1,11 @@
+<?php
+
+$string['chooseyear'] = 'Choose year';
+$string['nosyllabus'] = 'לא זמין סילבוס לקורס זה, אנא פנו אל המרצה לקבלת מידע נוסף';
+$string['syllabus'] = 'Syllabus';
+$string['mytimetable'] = 'לוח מפגשים בכיתות';
+$string['noclasses'] = 'מידע אודות מיקום המפגשים בכיתות אינו זמין, יש לפנות למזכירות למידע עדכני.';
+
+$string['logincheck'] = 'Please check you username and password';
+$string['logincheckpassword'] = 'You are not allowed to input Hebrew letters in the password';
+$string['notcalculated'] = 'Not calculated';
---
+++ lang/en/levinsky_he_backup.php
@@ -0,0 +1,11 @@
+<?php
+
+$string['chooseyear'] = 'בחרו שנה';
+$string['nosyllabus'] = 'לא זמין סילבוס לקורס זה, אנא פנו אל המרצה לקבלת מידע נוסף';
+$string['syllabus'] = 'סילבוס';
+$string['mytimetable'] = 'לוח מפגשים בכיתות';
+$string['noclasses'] = 'מידע אודות מיקום המפגשים בכיתות אינו זמין, יש לפנות למזכירות למידע עדכני.';
+
+$string['logincheck'] = 'אנא בדקו את שם המשתמש והסיסמה שלכם';
+$string['logincheckpassword'] = 'לא ניתן להזין תווים בעברית בסיסמה';
+$string['notcalculated'] = 'לא מחושב';
